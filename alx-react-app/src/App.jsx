11f790ef793d615708ed1b0a573012eb99483670
Jokotade <princesstade007@gmail.com>
2025-04-13 22:22:33 +0100

    welcome updated

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,17 @@
-import WelcomeMessage from './components/WelcomeMessage'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import WelcomeMessage from './components/WelcomeMessage';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-<WelcomeMessage/>
+<WelcomeMessage />
     </>
-  )
+  );
 }
 
 export default App
