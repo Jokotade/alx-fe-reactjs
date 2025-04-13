5eefa9642cd59ba5eb9103afd5c4fe4d8b97b1d1
Jokotade <princesstade007@gmail.com>
2025-04-13 22:50:25 +0100

    Header, maincontent and footer added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,8 @@
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
@@ -9,9 +13,13 @@ function App() {
 
   return (
     <>
-<WelcomeMessage />
+      <Header />
+      <WelcomeMessage />
+      <MainContent />
+      <Footer />
     </>
   );
+ 
 }
 
 export default App
